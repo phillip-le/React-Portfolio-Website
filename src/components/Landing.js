1040f1654b43a5604e1257588efbb821cfcff28e
Phillip Le <phillip.huy.le@gmail.com>
2021-03-05 14:40:08 +1000

    Added animation to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,22 @@
 import React from "react";
+import {Spring} from 'react-spring/renderprops';
+
 
 const Landing = () => {
-  return (
-    <div className="landing-text">
-      <h1>Phillip Le</h1>
-      <h6>Student Back End Developer and Data Engineer</h6>
-    </div>
-  );
+    return (
+        <Spring
+            from={{opacity: 0}}
+            to={{opacity: 1}}
+            config={{duration: 600}}
+        >
+            {props => (
+                <div className="landing-text" style={props}>
+                    <h1>Phillip Le</h1>
+                    <h6>Student Back End Developer and Data Engineer</h6>
+                </div>
+            )}
+        </Spring>
+    );
 };
 
 export default Landing;
